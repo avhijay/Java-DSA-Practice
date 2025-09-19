2649d7a8bca5b0053c1996d6c4145e8a6bfda69a
avhijay <parmaravhijay5049@gmail.com>
2025-09-19 23:39:39 +0530

    HashTable

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class HashTable {
 private int size = 7;
 
@@ -22,7 +24,7 @@ private  Node[] dataMap;
   }
 public void printHash(){
       for(int i = 0; i<dataMap.length;i++){
-          System.out.println(i+"::");
+          System.out.println(i+":");
           Node temp = dataMap[i];
 
           while(temp!=null){
@@ -39,10 +41,52 @@ private int hashing(String key){
       char[] keyChars = key.toCharArray();
       for(int i = 0; i<keyChars.length;i++){
           int asciiValue=keyChars[i];
-          hash = (hash+asciiValue*23);
+          hash = (hash+asciiValue*23)%dataMap.length; //%dataMap.length ensures that the hash always falls under the index boundaries;
 
       }
       //always going to be the number that is 0 to 6 ;
       return  hash;
 }
+public void set(String key, int value){
+      int index = hashing(key);
+      Node newNode = new Node(key,value);
+      if(dataMap[index]==null){
+          dataMap[index]=newNode;
+      }else{
+          Node temp = dataMap[index];
+          while(temp.next!=null){
+              temp = temp.next;
+          }
+          temp.next=newNode;
+      }
+}
+
+
+public int get(String key){
+      int index = hashing(key);
+      Node temp= dataMap[index];
+      while(temp!=null){
+          if(temp.key==key){
+              return temp.value;
+          }
+          temp=temp.next;
+
+      }
+return 0;
+
+}
+
+public ArrayList keys(){
+      ArrayList<String> allKeys=new ArrayList<>();
+      for (int i = 0; i<dataMap.length;i++){
+          Node temp = dataMap[i];
+          while(temp!=null){
+              allKeys.add(temp.key);
+              temp=temp.next;
+          }
+
+      }
+      return allKeys;
+}
+
 }
--- src/Main.java
+++ src/Main.java
@@ -54,8 +54,48 @@ DoublyLinkedList newOne = new DoublyLinkedList(9);
 
          // HASH TABLE:
          HashTable newHash = new HashTable();
-        newHash.printHash();
 
 
+        newHash.set("Mangoes",600);
+        newHash.set("Apple",89);
+         newHash.set("Litchi",788);
+//         newHash.printHash();
+         newHash.set("Grapes",655);
+         newHash.set("MILK",87);
+         newHash.set("Banana", 120);
+//         newHash.set("Orange", 340);
+//         newHash.set("Pineapple", 999);
+//         newHash.set("Strawberry", 150);
+//         newHash.set("Papaya", 432);
+//         newHash.set("Watermelon", 222);
+//         newHash.set("Peach", 77);
+//         newHash.set("Cherry", 505);
+//         newHash.set("Kiwi", 310);
+//         newHash.set("Guava", 274);
+//         newHash.set("Plum", 201);
+//         newHash.set("Apricot", 312);
+//         newHash.set("Blueberry", 415);
+//         newHash.set("Raspberry", 518);
+//         newHash.set("Blackberry", 622);
+//         newHash.set("Coconut", 725);
+//         newHash.set("Dates", 829);
+         newHash.set("Fig", 933);
+         newHash.set("Pomegranate", 1037);
+         newHash.set("Dragonfruit", 1140);
+        newHash.set("Jackfruit", 1244);         newHash.set("Durian", 1348);
+         newHash.set("Lychee", 1452);
+         newHash.set("Tamarind", 1556);
+         newHash.set("Mulberry", 1660);
+         newHash.set("Cranberry", 1764);
+         newHash.set("Gooseberry", 1868);
+          newHash.set("Lime", 2180);
+
+//         newHash.printHash();
+
+
+         System.out.println(newHash.get("Fig"));
+
+
+         System.out.println(newHash.keys());
      }
  }
