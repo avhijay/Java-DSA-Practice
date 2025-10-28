805b465b186990aac52f095bd1f0e5b21d0bb275
avhijay <parmaravhijay5049@gmail.com>
2025-10-28 23:35:55 +0530

    (MinHeaps) implementation

--- src/Heap.java
+++ src/Heap.java
@@ -4,96 +4,142 @@ import java.util.List;
 public class Heap {
 
 
-    private int leftChild(int index){
-        return 2*index+1;
+    private int leftChild(int index) {
+        return 2 * index + 1;
     }
 
-    private int rightChild(int index){
-        return 2*index+2;
-    }
-    private int parent(int index){
-        return(index-1)/2;
+    private int rightChild(int index) {
+        return 2 * index + 2;
     }
 
-
+    private int parent(int index) {
+        return (index - 1) / 2;
+    }
 
 
     private List<Integer> heap;
 
 
-    public  Heap(){
-        this.heap=new ArrayList<>();
+    public Heap() {
+        this.heap = new ArrayList<>();
     }
 
 
-    private void swap (int index1 , int index2 ){
+    private void swap(int index1, int index2) {
         int temp = heap.get(index1);
-        heap.set(index1,heap.get(index2));
-        heap.set(index2,temp);
+        heap.set(index1, heap.get(index2));
+        heap.set(index2, temp);
     }
 
 
-    public List<Integer> getHeap(){
+    public List<Integer> getHeap() {
         return new ArrayList<>(heap);
     }
 
 
-
-    public void insert(int value){
+    public void insert(int value) {
         heap.add(value);
-        int current = heap.size()-1;
+        int current = heap.size() - 1;
 
-        while(current>0&&heap.get(current)>heap.get(parent(current))){
-                swap(current,parent(current));
-                current = parent(current);
+        while (current > 0 && heap.get(current) > heap.get(parent(current))) {
+            swap(current, parent(current));
+            current = parent(current);
         }
 
     }
-    public Integer remove() {
-        if  (heap.size()==0)return null;
 
-        if (heap.size()==1){
-            return  heap.remove(0);
+    public Integer remove() {
+        if (heap.size() == 0) return null;
+
+        if (heap.size() == 1) {
+            return heap.remove(0);
         }
 
         int maxValue = heap.get(0);
-        heap.set(0,heap.remove(heap.size()-1));
+        heap.set(0, heap.remove(heap.size() - 1));
         sinkDown(0);
         return maxValue;
 
 
-
     }
 
-    public  void sinkDown(int index){
+    public void sinkDown(int index) {
         int maxIndex = index;
-        while(true){
-          int leftChild =  leftChild(index);
-          int rightChild = rightChild(index);
+        while (true) {
+            int leftChild = leftChild(index);
+            int rightChild = rightChild(index);
 
-          if(leftChild<heap.size() && heap.get(leftChild)>heap.get(maxIndex)){
-              maxIndex=leftChild;
-
-
-          }
-          if(rightChild<heap.size()&&  heap.get(rightChild)>heap.get(maxIndex)){
-              maxIndex=rightChild;
-          }
+            if (leftChild < heap.size() && heap.get(leftChild) > heap.get(maxIndex)) {
+                maxIndex = leftChild;
 
 
-          if (maxIndex!=index){
-              swap(index,maxIndex);
-              index=maxIndex;
+            }
+            if (rightChild < heap.size() && heap.get(rightChild) > heap.get(maxIndex)) {
+                maxIndex = rightChild;
+            }
 
-          }else{
-              return;
-          }
+
+            if (maxIndex != index) {
+                swap(index, maxIndex);
+                index = maxIndex;
+
+            } else {
+                return;
+            }
 
 
         }
     }
 
 
+    public void minHeapInsert(int value) {
+        heap.add(value);
+        int currentIndex = heap.size() - 1;
+        while (heap.get(parent(currentIndex)) > heap.get(currentIndex) && currentIndex > 0) {
+            swap(parent(currentIndex), currentIndex);
+            currentIndex = parent(currentIndex);
+        }
+    }
 
 
+    public Integer minHeapRemove(){
+        if(heap.size()==0)return  null;
+        if (heap.size()==1)return heap.remove(0);
+
+
+        int value = heap.get(0);
+        heap.set(0,heap.remove(heap.size()-1));
+        sinkDown(0);
+        return value;
+
+
+    }
+
+
+    public void minHeapSinkDown(int index) {
+        int minIndex = index;
+        while (true) {
+            int leftIndex = leftChild(index);
+            int rightIndex = rightChild(index);
+
+
+            if (leftIndex < heap.size() && heap.get(leftIndex) < heap.get(minIndex)) {
+                minIndex = leftIndex;
+            }
+
+            if (rightIndex < heap.size() && heap.get(rightIndex) < heap.get(minIndex)) {
+                minIndex = rightIndex;
+            }
+
+            if (minIndex != index) {
+                swap(index, minIndex);
+
+                index = minIndex;
+            } else {
+                return;
+            }
+
+
+        }
+    }
 }
