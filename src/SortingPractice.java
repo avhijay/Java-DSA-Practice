98e5366e8a7e8f48b685aee5589f08e64dbb2176
avhijay <parmaravhijay5049@gmail.com>
2025-11-26 23:30:49 +0530

    implement :  merge function , mergeSort

--- src/SortingPractice.java
+++ src/SortingPractice.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class SortingPractice {
 
 public static void bubbleSort(int [] array){
@@ -53,5 +55,71 @@ prev--;
     }
 
 }
+
+
+public static int [] merge( int [] num1  , int [] num2  ){
+
+    int i = 0 ;
+     int j = 0 ;
+     int index = 0 ;
+     int [] output = new int [num1.length+num2.length];
+
+    while (i < num1.length &&  j < num2.length ){
+        if(num1[i] > num2[j]){
+            output[index]=num2[j];
+             index++;
+            j++;
+        }else {
+            output[index] = num1[i];
+            index++;
+            i++;
+        }
+    }
+
+    while ( i < num1.length){
+
+        output[index]= num1[i];
+        index++;
+        i++;
+    }
+    while(j< num2.length){
+         output[index] = num2[j];
+         index++;
+         j++;
+    }
+ return  output;
+
+
+}
+
+
+ public static int [] mergeSort(int[]mergeMe ){
+
+    if (mergeMe.length==1)return mergeMe;
+
+     int mid = mergeMe.length/2;
+     int[] left = mergeSort(Arrays.copyOfRange(mergeMe,0,mid));
+     int [] right = mergeSort(Arrays.copyOfRange(mergeMe, mid,mergeMe.length));
+     return merge(left,right);
+
+
+ }
+
+
+ public static void main (String [] args ){
+
+    int [] newOne = {2,4,7,8,9,1,8,3,7};
+
+   int []  output = mergeSort(newOne);
+     System.out.println(Arrays.toString(output));
+
+
+ }
+
+
+
+
+
+
 
 }
